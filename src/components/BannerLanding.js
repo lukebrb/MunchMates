1402b799daf503e063827943598eb421b4fd4f53
Luke <lburbidg@ucsd.edu>
2019-12-12 01:33:34 -0800

    Change design

--- src/components/BannerLanding.js
+++ src/components/BannerLanding.js
@@ -1,17 +1,26 @@
 import React from 'react'
+import MunchmatesSVG from '../assets/images/munchmates.png'
+import styled from '@emotion/styled'
 
-const BannerLanding = (props) => (
-    <section id="banner" className="style2">
-        <div className="inner">
-            <header className="major">
-                <h1>Landing</h1>
-            </header>
-            <div className="content">
-                <p>Lorem ipsum dolor sit amet nullam consequat<br />
-                sed veroeros. tempus adipiscing nulla.</p>
-            </div>
-        </div>
-    </section>
+const Logo = () => <img src={MunchmatesSVG}></img>
+const MunchmatesLogo = styled(Logo)`
+  width: 300px !important;
+`
+
+const BannerLanding = props => (
+  <section id="banner" className="style2">
+    <div className="inner">
+      <header className="major">
+        <MunchmatesLogo />
+      </header>
+      <div className="content">
+        <p>
+          Our goal was to create the next best way for working professionals to
+          meet new people — at the dinner table.
+        </p>
+      </div>
+    </div>
+  </section>
 )
 
 export default BannerLanding
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,17 +2,14 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { Link } from 'gatsby'
 
-const Header = (props) => (
-    <header id="header" className="alt">
-        <Link to="/" className="logo"><strong>Forty</strong> <span>by HTML5 UP</span></Link>
-        <nav>
-            <a className="menu-link" onClick={props.onToggleMenu} href="javascript:;">Menu</a>
-        </nav>
-    </header>
+const Header = props => (
+  <header id="header" className="alt">
+    <Link to="/" className="logo"></Link>
+  </header>
 )
 
 Header.propTypes = {
-    onToggleMenu: PropTypes.func
+  onToggleMenu: PropTypes.func,
 }
 
 export default Header
