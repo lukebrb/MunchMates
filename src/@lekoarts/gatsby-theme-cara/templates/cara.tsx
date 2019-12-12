3cf48886f53778bd1ece71e6d7141051f871a1fe
Luke <lburbidg@ucsd.edu>
2019-12-11 22:48:38 -0800

    Fix imports

--- src/@lekoarts/gatsby-theme-cara/templates/cara.tsx
+++ src/@lekoarts/gatsby-theme-cara/templates/cara.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 import { Parallax } from "react-spring/renderprops-addons.cjs";
-import Layout from "../components/layout";
-import Hero from "../components/hero";
-import Projects from "../components/projects";
-import About from "../components/about";
-import Contact from "../components/contact";
+import Layout from "@lekoarts/gatsby-theme-cara/src/components/layout";
+import Hero from "@lekoarts/gatsby-theme-cara/src/components/hero";
+import Projects from "@lekoarts/gatsby-theme-cara/src/components/projects";
+import About from "@lekoarts/gatsby-theme-cara/src/components/about";
+import Contact from "@lekoarts/gatsby-theme-cara/src/components/contact";
 
 const Cara = () => (
   <Layout>
